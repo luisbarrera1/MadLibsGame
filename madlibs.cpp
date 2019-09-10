673c9631c4a45d9ad4e6c34f28daec1e95382361
luisbarrera1 <labarrera@mail.csuchico.edu>
2019-09-09 19:36:08 -0700

    created a mad libs by writing the proper code and was able to successfully play the mad lib game

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,30 @@
-//Authors: 
+//Authors: Luis Barrera
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+
+
 int main()
 {
+
+string name;
+string food;
+string verb;
+int number = 0; 
+
+cout<< "Enter a name:";
+cin>>name; 
+cout<< "Enter a food:";
+cin>> food;
+cout<< "Enter a verb:";
+cin>> verb; 
+cout<< "Enter a number:";
+cin>> number; 
+
+cout<< "There once wasa man named "<<name << ".He was a short fellow who loved to eat "<<food<< ".Every day he would go for a walk and "<<verb<< "every 5 min. He didnt have many friends except for his "<<number<< "cats.";
+
 
   return 0;
 }
